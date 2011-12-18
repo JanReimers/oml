cb175aca5b3df27e15ebff44da6581da8203a898
Jan Reimers <janreimers@gmail.com>
2011-12-18 22:29:27 +0000

    Move isnan
    git-svn-id: https://subversion.assembla.com/svn/celldesigntools/oml@208 e8b80fd4-3b04-4a2c-9396-e76f703d3c4f

---
+++ isnan.h
@@ -0,0 +1,65 @@
+#ifndef ISNAN_INCLUDED
+#define ISNAN_INCLUDED
+
+#include <oml/matrix.h>
+#include <oml/smatrix.h>
+#include <cmath>
+#include <complex>
+
+namespace std
+{
+template <class T> bool isnan(std::complex<T> c)
+{
+    return std::isnan(c.real()) || std::isnan(c.imag());
+}
+template <class T> bool isinf(std::complex<T> c)
+{
+    return std::isinf(c.real()) || std::isinf(c.imag());
+}
+}
+
+template <class T> inline bool isnan(const Matrix<T>& m)
+{
+    bool ret=false;
+    MatLimits lim=m.GetLimits();
+    for (int i=lim.Row.Low; i<=lim.Row.High; i++)
+        for (int j=lim.Col.Low; j<=lim.Col.High; j++)
+        {
+            if (std::isnan(m(i,j)) || std::isinf(m(i,j)))
+            {
+                ret=true;
+                break;
+            }
+        }
+    return ret;
+}
+
+template <class T> inline bool isnan(const SMatrix<T>& m)
+{
+    bool ret=false;
+    MatLimits lim=m.GetLimits();
+    for (int i=lim.Row.Low; i<=lim.Row.High; i++)
+        for (int j=i; j<=lim.Col.High; j++)
+        {
+            if (std::isnan(m(i,j)) || std::isinf(m(i,j)))
+            {
+                ret=true;
+                break;
+            }
+        }
+    return ret;
+}
+
+template <class T> inline bool isnan(const Vector<T>& v)
+{
+    bool ret=false;
+    VecLimits lim=v.GetLimits();
+    for (int i=lim.Low; i<=lim.High; i++)
+        if (std::isnan(v(i)) || std::isinf(v(i)))
+        {
+            ret=true;
+            break;
+        }
+    return ret;
+}
+#endif // ISNAN_INCLUDED
