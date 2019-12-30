2b4b0ab91d3069f115a0b7745cd118cccc878dec
Jan Reimers <janreimers@gmail.com>
2019-12-30 23:08:22 +0000

    Handle M<N for complex SVD
    git-svn-id: https://subversion.assembla.com/svn/celldesigntools/oml@1211 e8b80fd4-3b04-4a2c-9396-e76f703d3c4f

--- cnumeric.h
+++ cnumeric.h
@@ -51,26 +51,55 @@ template <class T> Vector<T> Diagonalize(Matrix<std::complex<T> >& A)
 
 extern"C" {
 typedef std::complex<double> eType;
-
+//
+//      A(MxN) = U(MxM) S(N) V*(NxN)
+//      M>=N
 void csvd_(eType* a, int* mmax, int* nmax, int* m, int* n, int* p, int* nu, int* nv,
 double* s, eType* u, eType* v );
 
 }
 
+
+using std::cout;
+using std::endl;
 //! sts::complex<double> version of SVDecomp
 template <class TM> void CSVDecomp(TM& A, Vector<double>& s, TM& V)
 {
     int M=A.GetNumRows();
     int N=A.GetNumCols();
+
+    bool transpose=N>M;
+    if (transpose)
+    {
+        int it=M;M=N;N=it;
+        TM temp=Transpose(A);
+        A.SetLimits(0,0);
+        A=temp; //TODO make a Transpose member function
+        V.SetLimits(N,N);
+    }
     assert(N<=M); //Required by fortran routine
     assert(M<=1000);  ////Required by fortran routine
-    assert(s.size()==M);
+    assert(s.size()==N);
     assert(V.GetNumRows()==N);
     assert(V.GetNumCols()==N);
     TM U(M,M);
     int p=0;
+//      A(MxN) = U(MxM) S(N) V*(NxN)
+//      M>=N
     csvd_ ( &A(1,1), &M, &N, &M, &N, &p, &N, &N, &s(1), &U(1,1), &V(1,1) );
-    A=U;
+    if (transpose)
+    {
+        A.SetLimits(0,0);
+        A=conj(V); //Store U in A for return
+        V.SetLimits(0,0);
+        V=conj(U);
+        V.SetLimits(M,N,true); //Reshape
+    }
+    else
+    {
+        A=U;
+        A.SetLimits(M,N,true); //Reshape
+    }
 }
 
 
