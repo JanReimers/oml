ecd24bda6f3f4ef97ff2e8125d73e44fcb03724c
Jan Reimers <janreimers@gmail.com>
2020-10-08 14:54:36 +0000

    Refactor: Have SVD routines return Vdagger instead of V
    git-svn-id: https://subversion.assembla.com/svn/celldesigntools/oml@1351 e8b80fd4-3b04-4a2c-9396-e76f703d3c4f

--- cnumeric.h
+++ cnumeric.h
@@ -88,47 +88,7 @@ double* s, eType* u, eType* v );
 using std::cout;
 using std::endl;
 //! std::complex<double> version of SVDecomp
-template <class TM> void CSVDecomp(TM& A, Vector<double>& s, TM& V)
-{
-    int M=A.GetNumRows();
-    int N=A.GetNumCols();
-
-    bool transpose=N>M;
-    if (transpose)
-    {
-        int it=M;M=N;N=it;
-        TM temp=Transpose(A);
-        A.SetLimits(0,0);
-        A=temp; //TODO make a Transpose member function
-        V.SetLimits(N,N);
-    }
-    assert(N<=M); //Required by fortran routine
-    assert(M<=1000);  ////Required by fortran routine
-    assert(s.size()==N);
-    assert(V.GetNumRows()==N);
-    assert(V.GetNumCols()==N);
-    TM U(M,M);
-    int p=0;
-//      A(MxN) = U(MxM) S(N) V*(NxN)
-//      M>=N
-    csvd_ ( &A(1,1), &M, &N, &M, &N, &p, &N, &N, &s(1), &U(1,1), &V(1,1) );
-    if (transpose)
-    {
-        A.SetLimits(0,0);
-        A=conj(V); //Store U in A for return
-        V.SetLimits(0,0);
-        V=conj(U);
-        V.SetLimits(M,N,true); //Reshape
-    }
-    else
-    {
-        A=U;
-        A.SetLimits(M,N,true); //Reshape
-    }
-}
-
-//! std::complex<double> version of SVDecomp
-template <class TM> std::tuple<TM,DiagonalMatrix<double>,TM> CSVDecomp(const TM& A)
+template <class TM> std::tuple<TM,DiagonalMatrix<double>,TM> oml_CSVDecomp(const TM& A)
 {
     int M=A.GetNumRows();
     int N=A.GetNumCols();
@@ -163,7 +123,8 @@ template <class TM> std::tuple<TM,DiagonalMatrix<double>,TM> CSVDecomp(const TM&
         U.SetLimits(0,0);
         U=temp;
     }
-    return std::make_tuple(U,DiagonalMatrix<double>(s),V);
+    TM Vdagger=Transpose(conj(V));
+    return std::make_tuple(U,DiagonalMatrix<double>(s),Vdagger);
 }
 
 
