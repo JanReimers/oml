0ee578f1e757521f817002030527db915351ac07
Jan Reimers <janreimers@gmail.com>
2020-10-23 17:54:05 -0500

    Support MP for smatrix mul

--- include/oml/imp/memops.h
+++ include/oml/imp/memops.h
@@ -156,8 +156,9 @@ void MatrixAssign(Indexable<T,Derived,Symmetric,D,MatrixShape>& a,const Indexabl
 #endif
   assert(a.GetLimits()==b.GetLimits());
   typename Derived::Subscriptor s(a);
-  for (index_t i:a.rows())
-    for (index_t j:a.rows(i))
+  #pragma omp parallel for collapse(2)
+  for (index_t i=a.GetLimits().Row.Low;i<=a.GetLimits().Row.High;i++)
+    for (index_t j=1;j<=a.GetLimits().Col.High;j++)
       s(i,j)=b(i,j);
 }
 
