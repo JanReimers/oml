482fb200635819be3cefd0f63727b49cc1199e72
Jan Reimers <janreimers@gmail.com>
2024-10-19 18:24:28 -0600

    Support conj(Vector3D<double>)

--- include/oml/fakedouble.h
+++ include/oml/fakedouble.h
@@ -8,13 +8,16 @@
 inline const double& conj(const double& d) { return d;}
 inline const double& real(const double& d) { return d;}
 inline       double  imag(const double& d) { return 0.0;}
+inline       double  Max (const double& d) { return d;}
 //using std::real;
 
 template <class T> class Matrix;
 template <class T> class Vector;
+template <class T> class Vector3D;
 
-inline const Matrix<double>& conj(const Matrix<double>& m) {return m;}
-inline const Vector<double>& conj(const Vector<double>& v) {return v;}
+inline const Matrix  <double>& conj(const Matrix  <double>& m) {return m;}
+inline const Vector  <double>& conj(const Vector  <double>& v) {return v;}
+inline const Vector3D<double>& conj(const Vector3D<double>& v) {return v;}
 
 
 #endif // FAKEDOUBLE_H_INCLUDED
