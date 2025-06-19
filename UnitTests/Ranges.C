92393246f0a6e9dc8cdbfbc9d487c57a0c0caf75
Jan Reimers <janreimers@gmail.com>
2025-06-19 11:41:41 -0600

    More examples
    generators and co_yield

--- UnitTests/Ranges.C
+++ UnitTests/Ranges.C
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <generator>
+#include <valarray>
 
 using std::cout;
 using std::endl;
@@ -48,6 +50,7 @@ template <std::ranges::range Range> void print(Range v)
     std::cout << "\n";
 }
 
+// Try out std::views::zip_transform
 TEST_F(RangesTests, zip_transform_Demo1)
 {
     std::vector<int> a = {1, 2, 3, 4, 5};
@@ -61,21 +64,9 @@ TEST_F(RangesTests, zip_transform_Demo1)
         EXPECT_EQ(pi,ai+bi);
     }
   
-    // cout << p << endl;
-    
-    // int left = 1;
-    // double right = 40.0;
-    // auto expr = zip_with(std::plus<>{}, std::ranges::single_view{left}, std::ranges::single_view{right});
-    // // print [41]
-    // std::cout << expr << std::endl;
-        
-    // short next_value = 1;
-    // auto expr2 = std::ranges::zip_view(std::plus<>{}, expr, std::ranges::single_view{next_value});
-        
-    // // print [42]
-    // std::cout << expr2 << std::endl;
 }
 
+// Try out std::views::zip_transform with singles.
 TEST_F(RangesTests, zip_transform_Demo2)
 {
     int left = 1;
@@ -91,3 +82,160 @@ TEST_F(RangesTests, zip_transform_Demo2)
     EXPECT_EQ(*expr2.begin(), 42);
     EXPECT_EQ(expr2[0], 42);
 }
+
+TEST_F(RangesTests, ScaledVectorAddition)
+{
+    std::vector<int> left_vals{1, 2, 3};
+    std::vector<int> right_vals{1, 1, 1};
+    auto alpha = 4;
+    auto scaled_rhs = right_vals | std::views::transform([&alpha](int val){return alpha * val;});
+    auto scaled_addition = std::views::zip_transform(std::plus<>{}, left_vals, scaled_rhs);
+
+    print(scaled_addition);
+    for (auto [pi,ai,bi] : std::views::zip(scaled_addition,left_vals,right_vals)) {
+        EXPECT_EQ(pi,ai+alpha*bi);
+    }
+    
+}
+
+// std::ranges::range is a concept (not a type).
+template <std::ranges::range Range> auto inner_product(const Range& a, const Range& b, double init)
+{
+    for (auto&& [ai, bi] : std::views::zip(a, b)) {
+        init += ai * bi;
+    }
+    return init;
+}
+
+TEST_F(RangesTests, MV_Mutiply1)
+{
+    using Matrix = std::vector<std::vector<double>>;
+    using Vector = std::vector<double>;
+
+    Matrix A = {{1, 2, 3}, {4, 5, 6}};
+    Vector x = {7,8, 9};
+    auto product = A | std::views::transform([&x](const auto& row){
+                            return inner_product(row, x, 0.0);
+                        });
+
+    EXPECT_EQ(product[0],50);
+    EXPECT_EQ(product[1],122);
+}
+
+//Try with vallarray instead of vector.
+TEST_F(RangesTests, MV_Mutiply2)
+{
+    using Matrix = std::valarray<std::valarray<double>>;
+    using Vector = std::valarray<double>;
+
+    Matrix A = {{1, 2, 3}, {4, 5, 6}};
+    Vector x = {7,8, 9};
+    auto product = A | std::views::transform([&x](const auto& row){
+                            return inner_product(row, x, 0.0);
+                        });
+
+    EXPECT_EQ(product[0],50);
+    EXPECT_EQ(product[1],122);
+}
+
+
+using Matrix = std::valarray<std::valarray<double>>;
+using Vector = std::valarray<double>;
+// Ax = B
+auto operator*(const Matrix& A, const Vector& x)
+{
+    return A | std::views::transform
+    ( [&x](const auto& row)
+        {
+            return inner_product(row, x, 0.0);
+        }
+    );
+}
+
+TEST_F(RangesTests, MV_Mutiply3)
+{
+    Matrix A = {{1, 2, 3}, {4, 5, 6}};
+    Vector x = {7,8, 9};
+    auto Ax=A*x;
+    EXPECT_EQ(Ax[0],50);
+    EXPECT_EQ(Ax[1],122);
+    std::vector<double> Ax2(Ax.begin(), Ax.end());
+    std::vector<double> Ax3(std::from_range,Ax); //c++-23
+    std::vector<double> Ax4(std::from_range,A*x); //c++-23
+}
+
+// std::generator<double> as_generator(const Vector& vec)
+// {
+//     for(double val : vec)
+//         co_yield val;
+// }
+
+std::vector<double> as_stdvector(std::generator<double> gen)
+{
+    std::vector<double> result;
+    for (auto val : gen) {
+        result.push_back(val);
+    }
+    return result;
+}
+
+std::generator<double> operator*(const Vector& vec, double factor)
+{
+    for(double val : vec)
+        co_yield val * factor;
+}
+std::generator<double> operator*(double factor,const Vector& vec)
+{
+    for(double val : vec)
+        co_yield factor * val;
+}
+
+std::generator<double> operator+( std::generator<double> lhs,  std::generator<double> rhs)
+{
+    auto ilhs=lhs.begin();
+    for (auto irhs=rhs.begin();irhs!=rhs.end();irhs++,ilhs++ )
+        co_yield *ilhs+*irhs;
+}
+
+TEST_F(RangesTests, GeneratorDemo)
+{
+    Vector v1 = {1, 2, 3};
+    Vector v2 = {4, 5, 6};
+    std::vector<double> result=as_stdvector(v1 * 2.0 + 3.0 * v2);
+    
+    print(result);
+    EXPECT_EQ(result.size(), 3);
+    EXPECT_EQ(result[0], 14); // 2*1 + 3*4
+    EXPECT_EQ(result[1], 19); // 2*2 + 3*5
+    EXPECT_EQ(result[2], 24); // 2*3 + 3*6
+}
+// // x*A = B
+// auto operator*(const Vector& x,const Matrix& A)
+// {
+//     return A | std::views::transform
+//     ( [&x](const auto& row)
+//         {
+//             return inner_product(row, x, 0.0);
+//         }
+//     );
+// }
+
+// auto operator*(const Matrix& A, const Matrix& B)
+// {
+//     auto product = A | std::views::transform([&B](const auto& row){
+//                           return row* B;
+//                        });
+//     return product;
+// }
+
+// TEST_F(RangesTests, MM_Mutiply1)
+// {
+//     Matrix A = {{1, 2, 3}, {4, 5, 6}};
+//     Matrix B = {{7, 8}, {9, 10}, {11, 12}};
+//     auto AB = A * B;
+//     print(AB[0]);
+//     // EXPECT_EQ(AB[0][0],58);
+//     // EXPECT_EQ(AB[0][1],64);
+//     // EXPECT_EQ(AB[1][0],139);
+//     // EXPECT_EQ(AB[1][1],154);
+// }
